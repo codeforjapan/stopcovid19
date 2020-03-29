576ed359c38a945a88d67e07a1aa11dd70de38d6
Hiroshi Omata <op.homata@gmail.com>
2020-03-29 15:31:51 +0900

    Add news card in about page and split map page

--- src/views/dashboard/about/About.vue
+++ src/views/dashboard/about/About.vue
@@ -4,6 +4,16 @@
     fluid
     tag="section"
   >
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-text>
+            <base-subheading subheading="お知らせ" />
+              当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、東京都が開設したものです。
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
 
-  </v-container>
+    </v-container>
 </template>
--- src/views/dashboard/maps/AreaMaps.vue
+++ src/views/dashboard/maps/AreaMaps.vue
@@ -6,23 +6,42 @@
   >
     <v-row>
       <v-col cols="12">
-        {{ news_title}}
+        {{ news_title }}
       </v-col>
     </v-row>
 
     <v-row>
-      <v-col cols="12">
-        <div>
-          {{ map_title }}
-        </div>
+      <v-col cols="6">
         <div id="map" />
       </v-col>
+      <v-col cols="6">
+        <v-data-table
+          dense
+          :headers="headers"
+          :items="areas"
+          item-key="prefecture"
+          class="elevation-1"
+          locale="ja-jp"
+          :pagination.sync="pagination"
+          hide-actions
+          loading-text="読込中"
+          no-data-text="データがありません。"
+        >
+          <template v-slot:item.production="{ item }">
+            <a
+              :href="item.production"
+              target="_blank"
+            >{{ item.production }}</a>
+          </template>
+        </v-data-table>
+      </v-col>
     </v-row>
-
   </v-container>
 </template>
 
 <script>
+  import DATA from '../../../../public/data/data.json'
+
   // Mapbox GL JSを読み込み
   import mapboxgl from 'mapbox-gl'
   import MapboxLanguage from '@mapbox/mapbox-gl-language'
@@ -33,6 +52,12 @@
       return {
         news_title: 'お知らせ: 東京都 新型コロナウイルス感染症対策サイト の派生サイトのマップです',
         map_title: '地域展開マップ',
+        areas: DATA,
+        headers: [
+          { text: '都道府県', sortable: true, value: 'prefecture' },
+          { text: '市区町村', sortable: true, value: 'city' },
+          { text: 'URL', sortable: true, value: 'production' },
+        ],
       }
     },
     mounted: function () {
@@ -113,6 +138,6 @@
   #map {
     z-index: 0;
     width: 100%;
-    height: 800px;
+    height: 400px;
   }
 </style>
