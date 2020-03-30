3faad1afb410aafaf578e6d4f6be6134e6c80741
Hiroshi Omata <op.homata@gmail.com>
2020-03-30 22:49:27 +0900

    Add vue-analytics

--- src/views/dashboard/tables/AreaTables.vue
+++ src/views/dashboard/tables/AreaTables.vue
@@ -11,6 +11,7 @@
             {{ news_title }}
           </h5>
         </base-subheading>
+        {{ update_title }}{{ update_date }}<br><br>
         <span v-html="news_note" />
       </v-col>
     </v-row>
@@ -34,6 +35,8 @@
     },
     data: () => ({
       news_title: 'StopCovid19 全国版の地域詳細一覧',
+      update_title: '最終更新：',
+      update_date: '2020-03-30 22:20',
       news_note: '東京都の<a href="https://github.com/tokyo-metropolitan-gov/covid19">オープンソース</a>' +
         'を活用した新型コロナウイルス感染症対策サイトの各都道府県のサイトをまとめています。',
     }),
