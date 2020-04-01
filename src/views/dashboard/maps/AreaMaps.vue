49d07d7ad4ed0e8955f70cf8b631f6d5c5580985
Hiroshi Omata <op.homata@gmail.com>
2020-04-01 13:52:30 +0900

    Update Nagano URL data

--- src/views/dashboard/maps/AreaMaps.vue
+++ src/views/dashboard/maps/AreaMaps.vue
@@ -41,7 +41,7 @@
       return {
         news_title: 'StopCovid19 全国版の地域マップ',
         update_title: '最終更新：',
-        update_date: '2020-03-30 22:20',
+        update_date: '2020-04-01 13:50',
         news_note: '東京都の<a href="https://github.com/tokyo-metropolitan-gov/covid19">オープンソース</a>' +
           'を活用した新型コロナウイルス感染症対策サイトの各都道府県のサイトをまとめています。',
       }
