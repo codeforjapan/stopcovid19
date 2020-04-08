146f2a9ef4a2bbdd6174cdafe2d4abea78d07dda
Hiroshi Omata <op.homata@gmail.com>
2020-04-08 14:36:27 +0900

    Update gunma pref and covid19 note

--- src/views/dashboard/maps/AreaMaps.vue
+++ src/views/dashboard/maps/AreaMaps.vue
@@ -41,7 +41,7 @@
       return {
         news_title: 'StopCovid19 全国版の地域マップ',
         update_title: '最終更新：',
-        update_date: '2020-04-03 13:23',
+        update_date: '2020-04-08 14:33',
         news_note: '東京都の<a href="https://github.com/tokyo-metropolitan-gov/covid19">オープンソース</a>' +
           'を活用した新型コロナウイルス感染症対策サイトの各都道府県のサイトをまとめています。',
       }
--- src/views/dashboard/tables/AreaTables.vue
+++ src/views/dashboard/tables/AreaTables.vue
@@ -36,7 +36,7 @@
     data: () => ({
       news_title: 'StopCovid19 全国版の地域詳細一覧',
       update_title: '最終更新：',
-      update_date: '2020-03-30 22:20',
+      update_date: '2020-04-08 14:34',
       news_note: '東京都の<a href="https://github.com/tokyo-metropolitan-gov/covid19">オープンソース</a>' +
         'を活用した新型コロナウイルス感染症対策サイトの各都道府県のサイトをまとめています。',
     }),
